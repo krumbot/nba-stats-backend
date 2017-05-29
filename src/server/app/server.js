6b8a585cdb26e2c2499c05529e9c61f46a152136
Vikrum Joshi <vikrum@Vikrums-MacBook-Pro.local>
2017-05-29 16:23:42 -0400

    with cors npm in place

--- src/server/app/server.js
+++ src/server/app/server.js
@@ -1,16 +1,15 @@
 import express from 'express';
 import apicache from 'apicache';
+import cors from 'cors';
 import fetchGames from './stats';
+
 
 const app = express();
 const cache = apicache.middleware;
 
 app.use(cache('1 minute'));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  return next();
-});
+app.use(cors());
 
 app.get('/scoreboard', (req, res) => {
   const { date } = req.query;
